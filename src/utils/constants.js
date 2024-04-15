4f23c36d633b302154cfbff76f04c7053defaee6
Quang Minh <nguyendoquangminh2112@gmail.com>
2024-04-15 21:33:58 +0700

    fix cors not allow local access server

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,6 @@ export const BOARD_TYPE = {
   PRIVATE: 'private'
 }
 
-export const WHITELIST_DOMAINS = ['http://localhost:5173/']
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173/'
+]
